5984db03b64cd452ce0d361621ec9754b62c520a
PadraicHeberlein <padraic.heb@gmail.com>
2024-10-06 18:47:20 -0400

    Added more styling and functionality to Common.jsx.

--- src/views/Commons.jsx
+++ src/views/Commons.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import "../styles/Commons.css";
+import emblem from "../svg/glasses-with-circular-lenses-svgrepo-com.svg";
+import doc from "../svg/book-closed-svgrepo-com.svg"
+import msg from "../svg/oscilloscope-svgrepo-com.svg"
+import ext from "../svg/calculator-svgrepo-com.svg"
 
 const Commons = {
     main: function() {
         return (
             <div className="main-view" >
-                <h1 className="title">COMMONS</h1>
+                <div className="title">
+                    <img src={emblem} alt="" />
+                    <h1 className="title">           
+                        COMMONS
+                    </h1>
+                </div>  
                 <ul className="links">
                     <li className="link">
-                        <a href="/commons/documentation">Documentation</a>
+                        <img src={doc} alt="" />
+                        <a href="/commons/documentation" className="text">Documentation</a>
                     </li>
                     <li className="link">
-                        <a href="/commons/message-boards">Message Boards</a>
+                        <img src={msg} alt="" />
+                        <a href="/commons/message-boards" className="text">Message Boards</a>
                     </li>
                     <li className="link">
-                        <a href="/commons/external-resources">External Resources</a>
+                        <img src={ext} alt="" />
+                        <a href="/commons/external-resources" className="text">External Resources</a>
                     </li>
                 </ul>
             </div>
